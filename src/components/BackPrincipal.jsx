fb38a08346aff9ff2d9227de54968f3ce530a0d8
Rosmery <rosmeryecc0112@gmail.com>
2024-09-15 14:06:47 -0400

    text changes

--- src/components/BackPrincipal.jsx
+++ src/components/BackPrincipal.jsx
@@ -6,10 +6,9 @@ const BackPrincipal = () => {
       <div className="background flex flex-col">
           <Header/>
           <div className="flex-grow flex flex-col justify-center p-4 md:p-8 relative z-10">
-              <h1 className="text-white text-xl sm:text-2xl md:text-4xl font-bold leading-tight mb-4">
-                  Adquiere de forma rápida y<br/>
-                  sencilla los productos que<br/>
-                  necesita
+              <h1 className="text-white text-xl sm:text-2xl md:text-4xl font-rounded leading-tight mb-4">
+                  Adquiere de forma rápida y sencilla<br/>
+                  los productos que necesita<br/>
               </h1>
           </div>
           <header className="p-3 bg-black bg-opacity-60 ">
