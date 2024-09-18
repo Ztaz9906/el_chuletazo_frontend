1f23452665647000d6b959843ce43a83a0ef24ea
Enrique Ferreiro <ztazhorde@gmail.com>
2024-09-17 22:00:44 -0400

    Agregado flujo de logOut y cambio en el navar de botones e iconos cuanod hay usuario o no

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,167 @@
-import '@/components/header/Header.css';
-import logo from '../../assets/logo.png';
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import logo from "../../assets/logo.png";
+import { LogOutIcon, Settings } from "lucide-react";
+import { useLogoutMutation } from "@/servicios/api/auth/logout/logout.js";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const user = useSelector((state) => state.user);
+  const [logout] = useLogoutMutation();
+  const navigate = useNavigate();
+  const toast = useToast();
+  const handleLogout = async () => {
+    try {
+      const response = await logout().unwrap();
+
+      toast({
+        title: response.detail,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+      navigate("/");
+    } catch (error) {
+      console.error("Error al intentar desconectarse:", error);
+    }
+  };
+
+  const getInitials = (firstName, lastName) => {
+    if (firstName && lastName) {
+      const firstLetter = firstName[0].toUpperCase();
+      const secondLetter = lastName[0].toUpperCase();
+      return `${firstLetter}${secondLetter}`;
+    } else if (firstName) {
+      return firstName[0].toUpperCase();
+    } else if (lastName) {
+      return lastName[0].toUpperCase();
+    }
+    return "U";
+  };
+
+  const menu = [
+    {
+      label: "Configuración",
+      onClick: () => {},
+      icon: <Settings />,
+    },
+    {
+      label: "Desconectarse",
+      onClick: handleLogout,
+      icon: <LogOutIcon />,
+    },
+  ];
+
   return (
-    <header className=" bg-black bg-opacity-30 flex justify-between p-2">
-      <div className="logo-container m-2">
-        <img src={logo} alt="Logo del negocio" className="logo w-13 h-10 object-contain"/>
-      </div>
-      <div className="button-container">
-        <button className="header-button">Iniciar Sesión</button>
-        <button className="header-button">Registrarse</button>
-      </div>
-    </header>
+    <Box bg="blackAlpha.300" p={2} height="56px">
+      <Flex justify="space-between" align="center" height="100%">
+        <Box m={2}>
+          <Image
+            src={logo}
+            alt="Logo del negocio"
+            h={"3rem"}
+            objectFit={"contain"}
+          />
+        </Box>
+        <Box>
+          {user ? (
+            <Flex align="center" gap={2}>
+              <Menu>
+                <MenuButton>
+                  <Flex alignItems="center" justify="start" gap={3}>
+                    <Avatar
+                      size="md"
+                      src=""
+                      name={getInitials(user.first_name, user.last_name)}
+                    />
+                    <VStack align="start" spacing={0} justifyContent="center">
+                      <Text
+                        color="white"
+                        fontWeight="medium"
+                        lineHeight="16px"
+                        fontSize={"14px"}
+                      >
+                        {`${user.first_name} ${user.last_name}`}
+                      </Text>
+                      <Text
+                        color="white"
+                        fontWeight="normal"
+                        lineHeight="12px"
+                        fontSize={"10px"}
+                      >
+                        {user.email}
+                      </Text>
+                    </VStack>
+                    <Icon
+                      viewBox="0 0 24 24"
+                      boxSize={"20px"}
+                      color="white"
+                      cursor="pointer"
+                    >
+                      <path fill="currentColor" d="M7 10l5 5 5-5z" />
+                    </Icon>
+                  </Flex>
+                </MenuButton>
+                <MenuList zIndex={50} bg={"blackAlpha.300"} border={"none"}>
+                  {menu.map((item, index) => (
+                    <MenuItem
+                      key={index}
+                      onClick={item.onClick}
+                      size="sm"
+                      color="white"
+                      bg={"transparent"}
+                      _hover={{ color: "green" }}
+                      gap={2}
+                    >
+                      {item.icon}
+                      {item.label}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </Menu>
+            </Flex>
+          ) : (
+            <>
+              <Button
+                className="header-button"
+                variant="ghost"
+                mr={2}
+                size="sm"
+                color="white"
+                _hover={{ bg: "transparent", color: "green" }}
+                onClick={() => navigate("/login")}
+              >
+                Iniciar Sesión
+              </Button>
+              <Button
+                className="header-button"
+                variant="ghost"
+                size="sm"
+                color="white"
+                _hover={{ bg: "transparent", color: "green" }}
+                onClick={() => navigate("/sign-up")}
+              >
+                Registrarse
+              </Button>
+            </>
+          )}
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
