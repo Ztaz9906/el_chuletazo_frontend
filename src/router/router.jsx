07decba2f4910fc835978ac29d2d993a4fb7da09
Enrique Ferreiro <ztazhorde@gmail.com>
2024-09-21 21:59:42 -0400

    Agregada ruta para la compra de pedido

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout.jsx";
 import Login from "@/components/auth/login/Login.jsx";
 import SingUp from "@/components/auth/sing-up/SingUp.jsx";
 import Index from "@/components/home/Productos/index.jsx";
-import CreatePedido from "@/components/home/pedidos/CreatePedido.js";
+import CreatePedido from "@/components/home/pedidos/CreatePedido.jsx";
 
 const router = createBrowserRouter([
   {
