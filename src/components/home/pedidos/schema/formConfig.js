1296b5d65a5b8995739ee785da931c87b5567bde
Enrique Ferreiro <ztazhorde@gmail.com>
2024-09-21 21:57:54 -0400

    Agregada ruta para la compra de pedido

---
+++ src/components/home/pedidos/schema/formConfig.js
@@ -0,0 +1,83 @@
+export const formConfig = {
+  destinatario: {
+    nombre: {
+      name: "nombre",
+      label: "Nombre",
+      errorMsg: "Ingrese el nombre del destinatario",
+    },
+    apellidos: {
+      name: "apellidos",
+      label: "Apellidos",
+      errorMsg: "Ingrese los apellidos del destinatario",
+    },
+    direccion: {
+      provincia: {
+        name: "provincia",
+        label: "Provincia",
+        errorMsg: "Seleccione una provincia",
+      },
+      municipio: {
+        name: "municipio",
+        label: "Municipio",
+        errorMsg: "Seleccione un municipio",
+      },
+      direccion: {
+        name: "direccion",
+        label: "Dirección",
+        errorMsg: "Ingrese una dirección válida",
+      },
+      numeroCasa: {
+        name: "numeroCasa",
+        label: "Número de casa",
+        errorMsg: "Ingrese el número de casa",
+      },
+    },
+    telefonoFijo: {
+      name: "telefonoFijo",
+      label: "Teléfono fijo",
+      errorMsg: "Ingrese un número de teléfono fijo válido",
+    },
+    telefonoCelular: {
+      name: "telefonoCelular",
+      label: "Teléfono celular",
+      errorMsg: "Ingrese un número de celular válido",
+    },
+    carneIdentidad: {
+      name: "carneIdentidad",
+      label: "Carné de Identidad",
+      errorMsg: "Ingrese un número de carné de identidad válido",
+    },
+    guardarDestinatario: {
+      name: "guardarDestinatario",
+      label: "Guardar destinatario para futuros envíos",
+      type: "checkbox",
+    },
+  },
+  usuarioEnvio: {
+    nombre: {
+      name: "nombreEnvio",
+      label: "Nombre",
+      errorMsg: "Ingrese su nombre",
+    },
+    apellidos: {
+      name: "apellidosEnvio",
+      label: "Apellidos",
+      errorMsg: "Ingrese sus apellidos",
+    },
+    direccion: {
+      name: "direccionEnvio",
+      label: "Dirección",
+      errorMsg: "Ingrese su dirección",
+    },
+    telefono: {
+      name: "telefonoEnvio",
+      label: "Teléfono",
+      errorMsg: "Ingrese su número de teléfono",
+    },
+    direccionFacturacionIgual: {
+      name: "direccionFacturacionIgual",
+      label: "La dirección de facturación es la misma que la de casa",
+      type: "checkbox",
+    },
+  },
+};
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout.jsx";
 import Login from "@/components/auth/login/Login.jsx";
 import SingUp from "@/components/auth/sing-up/SingUp.jsx";
 import Index from "@/components/home/Productos/index.jsx";
+import CreatePedido from "@/components/home/pedidos/CreatePedido.js";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         path: "productos",
         element: <Index />,
+      },
+      {
+        path: "pedidos",
+        element: <CreatePedido />,
       },
     ],
   },
