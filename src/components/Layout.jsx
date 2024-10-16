04b858ce5285569ddd13d69f94f526236fe25cc7
Enrique Ferreiro <ztazhorde@gmail.com>
2024-10-16 16:37:21 -0400

    arreglo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Box } from "..chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import CustomGoogleLogin from "../components/auth/google/GoogleLogin.jsx";
