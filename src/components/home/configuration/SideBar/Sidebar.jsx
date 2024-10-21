6f014ce35e0be5916388f1b44ce6e6eb80ea6916
Rosmery <rosmeryecc0112@gmail.com>
2024-10-21 16:30:11 -0400

    SideBar en Configuración

--- src/components/home/configuration/SideBar/Sidebar.jsx
+++ src/components/home/configuration/SideBar/Sidebar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
-import Menu from './Menu';
-
+import { Box, Flex, Text, Divider, Icon } from '@chakra-ui/react';
+import { FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+    const menuItems = [
+        { label: 'Datos Personales', href: '/datos-personales' },
+        { label: 'Cambio de Contraseña', href: '/cambio-contrasena' },
+        { label: 'Mis Destinatarios', href: '/configuracion/destinatarios' }
+    ];
+
     return (
-        <Box w="300px" bg="whiteAlpha.300" p="6" borderRadius="md" boxShadow="md">
-            <Flex direction="column" align="center">
-                <Text fontSize="2xl" mb="6">Configuración</Text>
-                <Menu />
+        <Box w="300px" bg="white" p="6" borderRadius="md" boxShadow="md">
+            <Flex direction="column" align="flex-start">
+                <Text fontSize="2xl" mb="6" alignSelf="center">Configuración</Text>
+                <Flex direction="column" align="flex-start" w="100%">
+                    <Icon as={FaUser} boxSize={10} mb="4" alignSelf="center" />
+                    <Text fontSize="xl" mb="4" alignSelf="center">Mi Cuenta</Text>
+                    {menuItems.map((item, index) => (
+                        <React.Fragment key={index}>
+                            <Link to={item.href}>
+                                <Text fontSize="lg" mb="2">{item.label}</Text>
+                            </Link>
+                            {index < menuItems.length - 1 && <Divider />}
+                        </React.Fragment>
+                    ))}
+                </Flex>
             </Flex>
         </Box>
     );
