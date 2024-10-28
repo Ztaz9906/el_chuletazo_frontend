abe53a790f3b79140a64b53cc9f8e6f0587f6ba6
Enrique Ferreiro <ztazhorde@gmail.com>
2024-10-28 04:25:17 -0400

    feat: popUp de google para el logeo

--- src/components/home/landingPage/LandingPage.jsx
+++ src/components/home/landingPage/LandingPage.jsx
@@ -1,66 +1,68 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import Header from "@/components/header/Header";
+import ImageCarousel from "@/components/home/landingPage/ImageCarousel";
 import {
   Box,
-  Container,
-  Heading,
-  Text,
   Button,
-  Stack,
+  Container,
   Grid,
+  Heading,
   Icon,
+  Stack,
+  Text,
   VStack,
-  useBreakpointValue
-} from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import { Heart, Package, Timer, Truck, ArrowRight } from 'lucide-react';
-import Header from '@/components/header/Header';
-import ImageCarousel from '@/components/home/landingPage/ImageCarousel';
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { ArrowRight, Heart, Package, Timer, Truck } from "lucide-react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CustomGoogleLogin from "../../auth/google/GoogleLogin";
 
 const fadeInUp = {
   initial: { opacity: 0.8, y: 30 },
-  animate: { 
+  animate: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.8, ease: "easeOut" }
+    transition: { duration: 0.8, ease: "easeOut" },
   },
   whileInView: { opacity: 1, y: 0 },
-  viewport: { once: true, amount: 0.3 }
+  viewport: { once: true, amount: 0.3 },
 };
 
 const scaleIn = {
   initial: { opacity: 0.8, scale: 0.95 },
-  animate: { 
+  animate: {
     opacity: 1,
     scale: 1,
-    transition: { duration: 0.8, ease: "easeOut" }
+    transition: { duration: 0.8, ease: "easeOut" },
   },
   whileInView: { opacity: 1, scale: 1 },
-  viewport: { once: true, amount: 0.3 }
+  viewport: { once: true, amount: 0.3 },
 };
 
 const LandingPage = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
-
+  const user = useSelector((state) => state.user);
   return (
     <Box>
+      {!user && <CustomGoogleLogin useOneTap={true} />}
       <Box
-        position="fixed" 
-        top="0" 
-        left="0" 
-        right="0" 
+        position="fixed"
+        top="0"
+        left="0"
+        right="0"
         zIndex="1000"
         bgGradient="linear(to-r, green.100, green.100)"
       >
-        <Header/>
+        <Header />
       </Box>
-           
-        <Box
-          position="relative"
-          height={{ base: "90vh", md: "80vh" }}
-          bgGradient="linear(to-b,  green.100, #fffaef)"
-          overflow="hidden"
-        >
+
+      <Box
+        position="relative"
+        height={{ base: "90vh", md: "80vh" }}
+        bgGradient="linear(to-b,  green.100, #fffaef)"
+        overflow="hidden"
+      >
         <Container maxW="container.xl" h="full" position="relative" zIndex="2">
           <Grid
             templateColumns={{ base: "1fr", md: "1fr 1fr" }}
@@ -82,8 +84,8 @@ const LandingPage = () => {
                 </Text>
               </Heading>
               <Text fontSize="xl" color="gray.600" mb={8}>
-                Lleva la alegría a tus seres queridos en Cuba con productos de calidad, 
-                entregados directamente a su puerta.
+                Lleva la alegría a tus seres queridos en Cuba con productos de
+                calidad, entregados directamente a su puerta.
               </Text>
               <Button
                 as={Link}
@@ -101,14 +103,21 @@ const LandingPage = () => {
               </Button>
             </motion.div>
 
-            <motion.div {...scaleIn} style={{ display: isMobile ? 'none' : 'block' }}>
-              <ImageCarousel/>
+            <motion.div
+              {...scaleIn}
+              style={{ display: isMobile ? "none" : "block" }}
+            >
+              <ImageCarousel />
             </motion.div>
           </Grid>
         </Container>
       </Box>
 
-      <Box py={20} bgGradient="linear(to-b , #fffaef, white)" position="relative">
+      <Box
+        py={20}
+        bgGradient="linear(to-b , #fffaef, white)"
+        position="relative"
+      >
         <Box
           position="absolute"
           top="0"
@@ -126,63 +135,56 @@ const LandingPage = () => {
               fontSize="4xl"
               color="green.600"
             >
-              ¿Por qué elegir {" "}
-                <Text as="span" color="cart.300">
-                  El Chuletazo?
-                </Text>
+              ¿Por qué elegir{" "}
+              <Text as="span" color="cart.300">
+                El Chuletazo?
+              </Text>
             </Heading>
           </motion.div>
 
-          <Grid
-            templateColumns={{ base: "1fr", md: "repeat(4, 1fr)" }}
-            gap={8}
-          >
+          <Grid templateColumns={{ base: "1fr", md: "repeat(4, 1fr)" }} gap={8}>
             {[
               {
                 icon: Heart,
                 title: "Calidad Garantizada",
-                description: "Seleccionamos cuidadosamente cada producto para asegurar la mejor calidad"
+                description:
+                  "Seleccionamos cuidadosamente cada producto para asegurar la mejor calidad",
               },
               {
                 icon: Package,
                 title: "Empaque Seguro",
-                description: "Embalaje especial para mantener los productos en perfectas condiciones"
+                description:
+                  "Embalaje especial para mantener los productos en perfectas condiciones",
               },
               {
                 icon: Truck,
                 title: "Entrega Confiable",
-                description: "Sistema de entrega eficiente directamente a la puerta en Cuba"
+                description:
+                  "Sistema de entrega eficiente directamente a la puerta en Cuba",
               },
               {
                 icon: Timer,
                 title: "Envío Rápido",
-                description: "Procesamiento y envío rápido para que llegue lo antes posible"
-              }
+                description:
+                  "Procesamiento y envío rápido para que llegue lo antes posible",
+              },
             ].map((feature, index) => (
-              <motion.div
-                key={index}
-                {...scaleIn}
-                style={{ height: '100%' }}
-              >
+              <motion.div key={index} {...scaleIn} style={{ height: "100%" }}>
                 <Box
                   p={6}
                   height="100%"
                   borderRadius="lg"
                   bg="green.50"
-                  shadow= "xl"
+                  shadow="xl"
                   _hover={{
                     transform: "translateY(-8px)",
                     shadow: "xl",
-                    bg: "#ffecc1"
+                    bg: "#ffecc1",
                   }}
                   transition="all 0.3s"
                 >
                   <VStack spacing={4} align="center">
-                    <Icon
-                      as={feature.icon}
-                      boxSize={12}
-                      color="cart.300"
-                    />
+                    <Icon as={feature.icon} boxSize={12} color="cart.300" />
                     <Heading size="md" color="cart.300">
                       {feature.title}
                     </Heading>
@@ -204,7 +206,7 @@ const LandingPage = () => {
           left="0"
           right="0"
           bottom="0"
-          bgGradient="linear(to-b, white, #fffaef)" 
+          bgGradient="linear(to-b, white, #fffaef)"
           backgroundPosition="center"
           zIndex="1"
         />
@@ -212,43 +214,35 @@ const LandingPage = () => {
         <Container maxW="container.xl" position="relative" zIndex="2">
           <motion.div {...fadeInUp}>
             <Stack spacing={8} align="center" mb={12}>
-              <Heading
-                textAlign="center"
-                color="green.500"
-                size="2xl"
-              >
+              <Heading textAlign="center" color="green.500" size="2xl">
                 Nuestros Productos
               </Heading>
-              <Text
-                textAlign="center"
-                fontSize="xl"
-                color="gray"
-                maxW="3xl"
-              >
-                Navega en nuestro sitio y descubre la selección de variados productos de alta calidad con la que contamos, 
-                elegidos cuidadosamente para satisfacer los gustos más exigentes de cada uno de nuestros clientes.
+              <Text textAlign="center" fontSize="xl" color="gray" maxW="3xl">
+                Navega en nuestro sitio y descubre la selección de variados
+                productos de alta calidad con la que contamos, elegidos
+                cuidadosamente para satisfacer los gustos más exigentes de cada
+                uno de nuestros clientes.
               </Text>
             </Stack>
           </motion.div>
         </Container>
       </Box>
 
-      <Box 
-      py={20} 
-      bgGradient="linear(to-b, #fffaef, green.100)" 
-      position="relative"
-      _before={{
-        content: '""',
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        height: "100px",
-        bgGradient: "linear(to-b, #fffaef, transparent)",
-        zIndex: 1
+      <Box
+        py={20}
+        bgGradient="linear(to-b, #fffaef, green.100)"
+        position="relative"
+        _before={{
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "100px",
+          bgGradient: "linear(to-b, #fffaef, transparent)",
+          zIndex: 1,
         }}
-        >
-
+      >
         <Container maxW="container.xl">
           <motion.div {...fadeInUp}>
             <Stack
@@ -262,7 +256,11 @@ const LandingPage = () => {
                   ¿Listo para hacer feliz a alguien?
                 </Heading>
                 <Text color="cart.300" fontSize="lg">
-                 <strong> Comienza a enviar productos de calidad a tus seres queridos HOY mismo. </strong>
+                  <strong>
+                    {" "}
+                    Comienza a enviar productos de calidad a tus seres queridos
+                    HOY mismo.{" "}
+                  </strong>
                 </Text>
               </VStack>
               <Button
