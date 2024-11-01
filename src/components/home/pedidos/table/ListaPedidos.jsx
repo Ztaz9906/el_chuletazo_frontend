c9fe778fedc16708f1c49b203edba4c5fba69b26
Rosmery <rosmeryecc0112@gmail.com>
2024-11-01 04:35:46 -0400

    Botones "Atrás" colocados en Lista de Pedidos

--- src/components/home/pedidos/table/ListaPedidos.jsx
+++ src/components/home/pedidos/table/ListaPedidos.jsx
@@ -1,6 +1,7 @@
 import PedidosTable from "@/components/home/pedidos/table/PedidosTable.jsx";
 import { useGetPedidosQuery } from "@/servicios/redux/api/Pedidos/index.js";
-import { Box, Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Divider, HStack, Text, VStack, Link } from "@chakra-ui/react";
+import { CircleChevronLeft } from "lucide-react";
 
 export default function ListaPedidos() {
   const { data, isLoading, isError, error } = useGetPedidosQuery();
@@ -13,7 +14,7 @@ export default function ListaPedidos() {
     <VStack
       h="calc(100vh - 100px)"
       bg="rgba(255, 255, 255, 0.6)"
-      p={4}
+      p={3}
       boxShadow="lg"
       mt={4}
       borderRadius={"5px"}
@@ -22,6 +23,23 @@ export default function ListaPedidos() {
         <Text fontSize="2xl" fontWeight="medium" color="main.600">
           Lista de Pedidos
         </Text>
+        <HStack
+            spacing={2}
+            justify={"space-between"}
+            as={Link}
+            href="/productos"
+            transition="transform 0.2s"
+            _hover={{
+              textDecoration: "none",
+              transform: "translateX(-5px)",
+              fontWeight: "bold",
+            }}
+          >
+            <Box as={CircleChevronLeft} size={20} color="main.600" />
+            <Text color="main.600" mr={"4px"}>
+              Atrás
+            </Text>
+          </HStack>
       </HStack>
       <Divider mb={4} borderColor="gray.300" />
       <Box borderWidth={1} borderRadius={"5px"} borderColor={"gray.300"}>
