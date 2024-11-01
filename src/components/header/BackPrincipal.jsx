4e550231ec9a2b74372c339deef6cb79a74a6165
Rosmery <rosmeryecc0112@gmail.com>
2024-10-31 22:37:04 -0400

    Cambios en el color del nav del Header

--- src/components/header/BackPrincipal.jsx
+++ src/components/header/BackPrincipal.jsx
@@ -15,7 +15,14 @@ const BackPrincipal = () => {
       flexDirection="column"
       color="white"
     >
-      <Box position="fixed" w="100%">
+      <Box
+        position="fixed"
+        top="0"
+        left="0"
+        right="0"
+        zIndex="1000"
+        backdropFilter="blur(10px)"
+      >
         <Header />
       </Box>
       <VStack
