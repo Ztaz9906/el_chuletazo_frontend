d9f0f5ec1677beacec29bf8c825b513ae5a90456
Enrique Ferreiro <ztazhorde@gmail.com>
2024-12-05 22:00:55 -0500

    fix: card de producto en steper

--- src/components/home/pedidos/Stepper/steps/productos/Product.jsx
+++ src/components/home/pedidos/Stepper/steps/productos/Product.jsx
@@ -1,6 +1,9 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Product({ product }) {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
   return (
     <Flex
       w="full"
@@ -9,39 +12,50 @@ export default function Product({ product }) {
       borderRadius="md"
       boxShadow="md"
       bg="white"
+      flexDirection="column"
     >
-      <Box w="100px" h="auto" mr={3} flexShrink={0}>
-        <Image
-          objectFit="cover"
-          w="100%"
-          h="100%"
-          src={product.image}
-          alt={product.name}
-        />
-      </Box>
-      <Box flex={1} p={1}>
-        <Text fontWeight="bold" fontSize="sm">
-          {product.name}
-        </Text>
-        <Text fontSize="xs" color="gray.500">
-          {product.description}
-        </Text>
-        <Text fontSize="xs" color="gray.600">
-          {product.quantity} x $
-          {(product.default_price.unit_amount / 100).toFixed(2)}{" "}
-          {product.default_price.currency.toUpperCase()}/unidad
-        </Text>
-        <Box textAlign="right">
-          <Text fontWeight="bold" fontSize="sm">
-            $
-            {(
-              (product.quantity * product.default_price.unit_amount) /
-              100
-            ).toFixed(2)}{" "}
-            {product.default_price.currency.toUpperCase()}
-          </Text>
+      <Flex h="100px">
+        <Box w="80px" h="full" mr={2} flexShrink={0}>
+          <Image
+            objectFit="cover"
+            w="100%"
+            h="100%"
+            src={product.image}
+            alt={product.name}
+          />
         </Box>
-      </Box>
+        <Flex flexDirection={"column"} justify={"space-between"} p={1}>
+          <Box flex={1} overflow="hidden">
+            <Text fontWeight="bold" fontSize="xs" noOfLines={1}>
+              {product.name}
+            </Text>
+
+            <Text
+              fontSize="2xs"
+              color="gray.500"
+              noOfLines={showFullDescription ? undefined : 2}
+            >
+              {product.description}
+            </Text>
+
+            <Text fontSize="2xs" color="gray.600" noOfLines={1}>
+              {product.quantity} x $
+              {(product.default_price.unit_amount / 100).toFixed(2)}{" "}
+              {product.default_price.currency.toUpperCase()}/unidad
+            </Text>
+          </Box>
+          <Flex textAlign="right" justify={"flex-end"}>
+            <Text fontWeight="bold" fontSize="xs" noOfLines={1}>
+              $
+              {(
+                (product.quantity * product.default_price.unit_amount) /
+                100
+              ).toFixed(2)}{" "}
+              {product.default_price.currency.toUpperCase()}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
