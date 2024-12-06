7088a6d000a3ef65bf40902c9804cf4b109944b9
Enrique Ferreiro <ztazhorde@gmail.com>
2024-12-05 21:27:10 -0500

    fix: card de productos en el detalle de pedidos

--- src/components/home/pedidos/details/PedidoProductsMobile.jsx
+++ src/components/home/pedidos/details/PedidoProductsMobile.jsx
@@ -1,50 +1,45 @@
-import { Box, Divider, Grid, HStack, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 export default function PedidoProductsMobile({ item }) {
   const { producto, cantidad } = item;
   const precioUnitario = producto.default_price.unit_amount / 100;
   const subtotal = precioUnitario * cantidad;
-
   return (
-    <Box borderWidth={1} borderRadius="lg" bg="white" shadow="sm">
-      <HStack justify="center" p={2}>
-        <Box h={{ base: "auto", md: "200px" }} w={{ base: "60%", md: "auto" }}>
-          <img
-            src={producto.image}
-            alt={producto.name}
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        </Box>
-        <Box
-          p={4}
-          h={{ base: "auto", md: "200px" }}
-          w={{ base: "40%", md: "auto" }}
-        >
-          <Text fontSize="lg" fontWeight="semibold" mb={2}>
-            {producto.name}
-          </Text>
-          {producto.description && (
-            <Text fontSize="sm" color="gray.600" mb={2}>
-              {producto.description}
-            </Text>
-          )}
-        </Box>
-      </HStack>
-      <Divider my={2} />
-      <Grid templateColumns="1fr 1fr" gap={2} fontSize="sm" p={2}>
-        <Text>Precio:</Text>
-        <Text textAlign="right">${precioUnitario}</Text>
-        <Text>Cantidad:</Text>
-        <Text textAlign="right">{cantidad}</Text>
-        <Text fontWeight="bold">Subtotal:</Text>
-        <Text textAlign="right" fontWeight="bold">
-          ${subtotal}
+    <Flex
+      w="full"
+      border="1px solid"
+      borderColor="gray.200"
+      borderRadius="md"
+      boxShadow="md"
+      bg="white"
+    >
+      <Box w="100px" h="auto" mr={3} flexShrink={0}>
+        <Image
+          objectFit="cover"
+          w="100%"
+          h="100%"
+          src={producto.image}
+          alt={producto.name}
+        />
+      </Box>
+      <Box flex={1} p={1}>
+        <Text fontWeight="bold" fontSize="sm">
+          {producto.name}
         </Text>
-      </Grid>
-    </Box>
+        <Text fontSize="xs" color="gray.500">
+          {producto.description}
+        </Text>
+        <Text fontSize="xs" color="gray.600">
+          {cantidad} x ${(producto.default_price.unit_amount / 100).toFixed(2)}{" "}
+          {producto.default_price.currency.toUpperCase()}/lbs
+        </Text>
+        <Box textAlign="right">
+          <Text fontWeight="bold" fontSize="sm">
+            ${subtotal.toFixed(2)}{" "}
+            {producto.default_price.currency.toUpperCase()}
+          </Text>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
